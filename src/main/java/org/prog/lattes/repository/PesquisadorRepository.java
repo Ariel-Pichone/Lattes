0de40a33108360c845438c0785e5f65b7123bf34
diegoamelo <diegoalencar10@gmail.com>
2023-06-06 02:21:55 -0300

    Paginação

---
+++ src/main/java/org/prog/lattes/repository/PesquisadorRepository.java
@@ -0,0 +1,28 @@
+package org.prog.lattes.repository;
+
+import java.util.List;
+import org.prog.lattes.model.Pesquisador;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PesquisadorRepository extends JpaRepository<Pesquisador, String>, JpaSpecificationExecutor<Pesquisador> {
+
+    public static Specification<Pesquisador> filtrarPorIdentificador(String identificador) {
+        return (root, query, builder) -> builder.like(builder.lower(root.get("identificador")), "%" + identificador + "%");
+    }
+    
+    public static Specification<Pesquisador> filtrarPorNome(String nome) {
+        return (root, query, builder) -> builder.like(builder.lower(root.get("nome")), "%" + nome.toLowerCase() + "%");
+    }
+
+    @Query("SELECT p.nome FROM Pesquisador p WHERE p.instituto = 'LIVRO'")
+    long countLivro();
+    
+    List<Pesquisador> findByIdentificador(String identificador);
+      
+    Boolean existsByIdentificador(String identificador);
+}
