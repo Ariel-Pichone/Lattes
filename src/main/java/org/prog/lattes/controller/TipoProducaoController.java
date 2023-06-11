e41cbe04f3811485ca389f448300a4d48cf9076a
diegoamelo <diegoalencar10@gmail.com>
2023-06-11 14:27:26 -0300

    Filtro para retornar os tipos de produção

--- src/main/java/org/prog/lattes/controller/TipoProducaoController.java
+++ src/main/java/org/prog/lattes/controller/TipoProducaoController.java
@@ -1,9 +1,10 @@
 package org.prog.lattes.controller;
 
-import java.util.List;
 import org.prog.lattes.model.TipoProducao;
 import org.prog.lattes.service.TipoProducaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class TipoProducaoController {
     }
 
     @GetMapping
-    public List<TipoProducao> listTipo() {
-        return tipoProducaoService.listTipoProducao();
+    public Page<String> pageTipoProducao(Pageable pageable) {
+        return tipoProducaoService.pageTipoProducao(pageable);
     }
 }
--- src/main/java/org/prog/lattes/service/TipoProducaoService.java
+++ src/main/java/org/prog/lattes/service/TipoProducaoService.java
@@ -2,15 +2,34 @@ package org.prog.lattes.service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.jvnet.hk2.annotations.Service;
 import org.prog.lattes.model.TipoProducao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Service
 @Component
 public class TipoProducaoService {
     
-    public List<TipoProducao> listTipoProducao(){
-        return Arrays.asList(TipoProducao.values());
+    public Page<String> pageTipoProducao(Pageable pageable){
+        //Usado para ordenar a pagina pelo nome da produção de forma crescente
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("nome"));
+        
+        List<String> tipoProducoes = Arrays.stream(TipoProducao.values())
+                                        .map(TipoProducao::getNome)
+                                        .collect(Collectors.toList());
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), tipoProducoes.size());
+
+        List<String> tipoProducoesPaginados = tipoProducoes.subList(start, end);
+        Page<String> page = new PageImpl<>(tipoProducoesPaginados, pageRequest, tipoProducoes.size());
+
+        return page;
     }
 }
